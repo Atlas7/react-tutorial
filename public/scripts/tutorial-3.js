049fd7ce90c914f525c224b59a876421012e3bb5
atlas7 <johnnychan0302@gmail.com>
2016-04-14 15:53:01 +0100

    Skeleton CommentBox, CommentList, and CommentForm

---
+++ public/scripts/tutorial-3.js
@@ -0,0 +1,15 @@
+/**
+ * Created by johnny on 14/04/2016.
+ */
+// tutorial3.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
---
+++ public/scripts/tutorial-6.js
@@ -0,0 +1,16 @@
+/**
+ * Created by johnny on 14/04/2016.
+ */
+// tutorial6.js
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {marked(this.props.children.toString())}
+      </div>
+    );
+  }
+});
